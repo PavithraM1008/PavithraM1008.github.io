1beac4fbd66b8f48e44a0b8b815e19963c3e125b
PavithraM1008 <pavithra.kezi10@gmail.com>
2020-03-01 17:26:58 +0530

    Added first site

---
+++ precache-manifest.90b452c053db5ed03ceffac22f0f5e95.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ef64abb4c5b0d943c2c189f433103e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a246a8d4969c91feb64",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "adb2d8b66cdbcc0bc7d4",
+    "url": "/static/css/main.3b03d288.chunk.css"
+  },
+  {
+    "revision": "7a246a8d4969c91feb64",
+    "url": "/static/js/2.be15fd50.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/static/js/2.be15fd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb2d8b66cdbcc0bc7d4",
+    "url": "/static/js/main.2cb35071.chunk.js"
+  },
+  {
+    "revision": "4f8b734a7e80109b6f7e",
+    "url": "/static/js/runtime-main.10a268e8.js"
+  },
+  {
+    "revision": "46ff062423996615d7a6bf8c11f48eba",
+    "url": "/static/media/83598a5ab3f5371e7662b565836c0c39.46ff0624.jpg"
+  },
+  {
+    "revision": "efa3ed5c152e50f57b5d10ff42183ae8",
+    "url": "/static/media/check id.efa3ed5c.jpg"
+  }
+]);
